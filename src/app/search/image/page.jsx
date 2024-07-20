c7642687fca189a6a6b9636044772f1f3feff950
Parthik Diyora <ddiyora503@gmail.com>
2024-07-20 23:09:47 +0530

    changing components......

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -5,7 +5,7 @@ export default async function ImageSearchPage({ searchParams }) {
   const startIndex = searchParams.start || "1";
   await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&searchType=image&start=${startIndex}`
+    ` https://www.googleapis.com/customsearch/v1?key=INSERT_YOUR_API_KEY&cx=017576662512468239146:omuauf_lfve&q=${searchParams.searchTerm}'}&searchType=image&start=${startIndex}`
   );
   if (!response.ok) throw new Error("Something went wrong");
   const data = await response.json();
